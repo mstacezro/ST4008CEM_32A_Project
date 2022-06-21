b3f7b6cad717754b86f9fa054f25b14bd67545d8
Asmit11 <102939724+Asmit11@users.noreply.github.com>
2022-06-21 15:22:00 +0545

    Add files via upload
    staff and product table added

--- Codes/staff_details.py
+++ Codes/staff_details.py
@@ -1,8 +1,7 @@
-#import sqlite3
 import sqlite3
 # DATABASES
 #create a database or connect to one
-conn=sqlite3.connect('staff.db')
+conn=sqlite3.connect('manager.db')
 
 #create a cursor
 '''
@@ -11,6 +10,60 @@ SQLite3 statements, fetch data from the result sets of the queries
 '''
 c=conn.cursor()
 
-'''CREATE command is used to create a new SQLite database named "staff". '''
 
-# print("Staff Table is created")
+#manager table
+
+'''CREATE command is used to create a new SQLite database named "manager. '''
+c.execute("""CREATE TABLE manager(
+    first_name text,
+    last_name text,
+    father_name text,
+    DOB text,
+    Phone integer,
+    address text,
+    city text,
+    provine text,
+    zipcode integer,
+    password text,
+    re_password text,
+    manager_id integer
+    )""")
+print("Manager Table is is created.")
+
+#.......................
+
+
+
+#staff table
+
+'''CREATE command is used to create a new SQLite database named "staff. '''
+c.execute("""CREATE TABLE staff(
+    first_name text,
+    last_name text,
+    father_name text,
+    DOB text,
+    Phone integer,
+    address text,
+    city text,
+    provine text,
+    zipcode integer,
+    password text,
+    re_password text,
+    staff_id integer
+    )""")
+print("Staff Table is is created.")
+
+#.......................
+
+#product table
+
+'''CREATE command is used to create a new SQLite database named "product. '''
+c.execute("""CREATE TABLE product(
+    product_name text,
+    product_id integer
+    )""")
+print("Product Table is is created.")
+
+#.......................
+
+
