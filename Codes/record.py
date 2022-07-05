3a73752e0183c2ddbb7a50d53da7d8556f1cefa0
Asmit11 <102939724+Asmit11@users.noreply.github.com>
2022-07-05 14:45:53 +0545

    Add files via upload
    File created instead of staff details and product details

---
+++ Codes/record.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+'''
+cursor class is an instance using which you can invoke methods that execute 
+SQLite3 statements, fetch data from the result sets of the queries
+'''
+
+'''
+Manager DB
+'''
+conn=sqlite3.connect('RECORD.db')
+c=conn.cursor()
+# Creating Table
+c.execute("""CREATE TABLE Manager(
+    f_name text,
+    l_name text,
+    age integer,
+    gender text,
+    pin integer,
+    re_pin integer,
+    father_name text,
+    phone integer,
+    address text,
+    city text,
+    zipcode integer,
+    Manager_id integer,
+)""")
+
+'''
+Staff DB
+'''
+c.execute("""CREATE TABLE Staff(
+    f_name text,
+    l_name text,
+    age integer,
+    gender text,
+    pin integer,
+    re_pin integer,
+    father_name text,
+    phone integer,
+    address text,
+    city text,
+    zipcode integer,
+    Staff_id integer
+)""")
+
+print("Table Created")
+
+
+'''
+Product DB
+'''
+c.execute("""CREATE TABLE Product(
+    product_name text,
+    product_price integer,
+    product_id integer
+)""")
+
+print("Table Created")
